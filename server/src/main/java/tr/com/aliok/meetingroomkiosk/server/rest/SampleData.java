b408c37990bf28abd7ffcf3c3dce9112c94600de
Ali Ok <aliok@apache.org>
2014-09-11 00:58:17 +0200

    Better sample data

--- server/src/main/java/tr/com/aliok/meetingroomkiosk/server/rest/SampleData.java
+++ server/src/main/java/tr/com/aliok/meetingroomkiosk/server/rest/SampleData.java
@@ -1,12 +1,12 @@
 package tr.com.aliok.meetingroomkiosk.server.rest;
 
-import org.apache.commons.lang3.time.DateUtils;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
 import tr.com.aliok.meetingroomkiosk.model.api.AttendeeStatus;
 import tr.com.aliok.meetingroomkiosk.model.api.PeriodType;
 import tr.com.aliok.meetingroomkiosk.server.model.*;
 
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -15,11 +15,16 @@ import java.util.List;
  */
 public class SampleData {
 
-    public final Date now = new Date();
-    public final Date today_00oclock = DateUtils.truncate(now, Calendar.DAY_OF_MONTH);
-    public final Date today_08oclock = DateUtils.addHours(today_00oclock, 8);
-    public final Date today_12oclock = DateUtils.addHours(today_00oclock, 12);
-    public final Date today_24oclock = DateUtils.ceiling(now, Calendar.DAY_OF_MONTH);
+    public final DateTime now = new DateTime();
+    public final DateTime monday_00oclock = now.withDayOfWeek(DateTimeConstants.MONDAY).withTimeAtStartOfDay();
+    public final DateTime tuesday_00oclock = now.withDayOfWeek(DateTimeConstants.TUESDAY).withTimeAtStartOfDay();
+    public final DateTime wednesday_00oclock = now.withDayOfWeek(DateTimeConstants.WEDNESDAY).withTimeAtStartOfDay();
+    public final DateTime thursday_00oclock = now.withDayOfWeek(DateTimeConstants.THURSDAY).withTimeAtStartOfDay();
+    public final DateTime friday_00oclock = now.withDayOfWeek(DateTimeConstants.FRIDAY).withTimeAtStartOfDay();
+    public final DateTime friday_24oclock = monday_00oclock.withDayOfWeek(DateTimeConstants.FRIDAY).plusDays(1).minusMillis(1);
+
+    public final DateTime today_00oclock = now.withTimeAtStartOfDay();
+    public final DateTime today_24oclock = today_00oclock.plusDays(1).minusMillis(1);
 
     public final Attendee attendee1 = new Attendee.Builder()
             .setAttendeeStatus(AttendeeStatus.ACCEPTED)
@@ -38,15 +43,81 @@ public class SampleData {
 
     public final Attendee organizer1 = attendee1;
 
-    public final Event event1 = new Event.Builder()
-            .setEventKey("ABC-DEF")
+    public final Event event_today_08oclock = new Event.Builder()
+            .setEventKey("event_today_08oclock")
             .setAllDayEvent(false)
             .setAttendees(Arrays.asList(attendee1, attendee2))
             .setEventTitle("Innbound Sprint Planning Meeting")
-            .setEventDescription("Let's do it on 14:30 since Christoph is not available in the morning")
+            .setEventDescription("Let's do it on 08:30 since Christoph is not available in the morning")
             .setOrganizer(organizer1)
-            .setEventStart(today_08oclock)
-            .setEventEnd(today_12oclock)
+            .setEventStart(today_00oclock.withHourOfDay(8).toDate())
+            .setEventEnd(today_00oclock.withHourOfDay(11).toDate())
+            .create();
+
+    public final Event event_today_15oclock = new Event.Builder()
+            .setEventKey("event_today_15oclock")
+            .setAllDayEvent(false)
+            .setAttendees(Arrays.asList(attendee1, attendee2))
+            .setEventTitle("Book club")
+            .setEventDescription("Join the club")
+            .setOrganizer(organizer1)
+            .setEventStart(today_00oclock.withHourOfDay(15).toDate())
+            .setEventEnd(today_00oclock.withHourOfDay(16).withMinuteOfHour(15).toDate())
+            .create();
+
+    public final Event event_monday_12oclock = new Event.Builder()
+            .setEventKey("event_monday_12oclock")
+            .setAllDayEvent(false)
+            .setAttendees(Arrays.asList(attendee1, attendee2))
+            .setEventTitle("Review meeting")
+            .setEventDescription("Review and demo")
+            .setOrganizer(organizer1)
+            .setEventStart(monday_00oclock.withHourOfDay(12).toDate())
+            .setEventEnd(monday_00oclock.withHourOfDay(13).withMinuteOfHour(30).toDate())
+            .create();
+
+    public final Event event_tuesday_11oclock = new Event.Builder()
+            .setEventKey("event_tuesday_11oclock")
+            .setAllDayEvent(false)
+            .setAttendees(Arrays.asList(attendee1, attendee2))
+            .setEventTitle("Review meeting")
+            .setEventDescription("Review and demo")
+            .setOrganizer(organizer1)
+            .setEventStart(tuesday_00oclock.withHourOfDay(11).toDate())
+            .setEventEnd(tuesday_00oclock.withHourOfDay(13).withMinuteOfHour(0).toDate())
+            .create();
+
+    public final Event event_wednesday_11oclock = new Event.Builder()
+            .setEventKey("event_wednesday_11oclock")
+            .setAllDayEvent(false)
+            .setAttendees(Arrays.asList(attendee1, attendee2))
+            .setEventTitle("Scrum meeting")
+            .setEventDescription("Daily scrum")
+            .setOrganizer(organizer1)
+            .setEventStart(wednesday_00oclock.withHourOfDay(11).toDate())
+            .setEventEnd(wednesday_00oclock.withHourOfDay(14).withMinuteOfHour(0).toDate())
+            .create();
+
+    public final Event event_thursday_11oclock = new Event.Builder()
+            .setEventKey("event_thursday_11oclock")
+            .setAllDayEvent(false)
+            .setAttendees(Arrays.asList(attendee1, attendee2))
+            .setEventTitle("Scrum meeting")
+            .setEventDescription("Daily scrum")
+            .setOrganizer(organizer1)
+            .setEventStart(thursday_00oclock.withHourOfDay(11).toDate())
+            .setEventEnd(thursday_00oclock.withHourOfDay(14).withMinuteOfHour(0).toDate())
+            .create();
+
+    public final Event event_friday_11oclock = new Event.Builder()
+            .setEventKey("event_friday_11oclock")
+            .setAllDayEvent(false)
+            .setAttendees(Arrays.asList(attendee1, attendee2))
+            .setEventTitle("Scrum meeting")
+            .setEventDescription("Daily scrum")
+            .setOrganizer(organizer1)
+            .setEventStart(friday_00oclock.withHourOfDay(11).toDate())
+            .setEventEnd(friday_00oclock.withHourOfDay(14).withMinuteOfHour(0).toDate())
             .create();
 
     public final Room room1 = new Room.Builder()
@@ -55,23 +126,49 @@ public class SampleData {
             .create();
 
 
-    public final List<Event> events = Arrays.asList(event1);
+    public final List<Event> dayEvents = Arrays.asList(event_today_08oclock, event_today_15oclock);
+
+    public final List<Event> weekEvents = Arrays.asList(
+            event_today_08oclock,
+            event_today_15oclock,
+            event_monday_12oclock,
+            event_tuesday_11oclock,
+            event_wednesday_11oclock,
+            event_thursday_11oclock,
+            event_friday_11oclock);
 
     public final Schedule daySchedule = new Schedule.Builder()
             .setRoom(room1)
             .setScheduleServerSyncTime(new Date())
-            .setEvents(events)
+            .setEvents(dayEvents)
+            .create();
+
+    public final Schedule weekSchedule = new Schedule.Builder()
+            .setRoom(room1)
+            .setScheduleServerSyncTime(new Date())
+            .setEvents(weekEvents)
             .create();
 
     public final Period dayPeriod = new Period.Builder()
-            .setStartDateTime(today_00oclock)
-            .setEndDateTime(today_24oclock)
+            .setStartDateTime(today_00oclock.toDate())
+            .setEndDateTime(today_24oclock.toDate())
             .setPeriodType(PeriodType.DAY)
             .create();
 
-    public final PeriodSchedule periodSchedule = new PeriodSchedule.Builder()
+    public final Period weekPeriod = new Period.Builder()
+            .setStartDateTime(monday_00oclock.toDate())
+            .setEndDateTime(friday_24oclock.toDate())
+            .setPeriodType(PeriodType.WEEK)
+            .create();
+
+    public final PeriodSchedule dayPeriodSchedule = new PeriodSchedule.Builder()
             .setPeriod(dayPeriod)
             .setSchedule(daySchedule)
+            .create();
+
+    public final PeriodSchedule weekPeriodSchedule = new PeriodSchedule.Builder()
+            .setPeriod(weekPeriod)
+            .setSchedule(weekSchedule)
             .create();
 
     public final Sensor sensor1 = new Sensor.Builder()
@@ -85,6 +182,8 @@ public class SampleData {
             .setSensor(sensor1)
             .create();
 
-    public final ScheduleInformation scheduleInformation = new ScheduleInformation.Builder().setPeriodSchedules(Arrays.asList(periodSchedule)).create();
+    public final ScheduleInformation scheduleInformation = new ScheduleInformation.Builder()
+            .setPeriodSchedules(Arrays.asList(dayPeriodSchedule, weekPeriodSchedule))
+            .create();
 
 }
