1e86e2e972cda92f0c3e0ab05531d99f46bc4e49
Ali Ok <aliok@apache.org>
2014-09-13 02:09:32 +0200

    More date options according to the sample data

--- android/app/src/main/java/tr/com/aliok/meetingroomkiosk/android/util/Clock.java
+++ android/app/src/main/java/tr/com/aliok/meetingroomkiosk/android/util/Clock.java
@@ -15,17 +15,23 @@ public class Clock {
     // for the 'Z' at the end
     private final static DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
-    private static final long DATETIME_1;       // in interval of event_today_08oclock
+    private static final long DATETIME_1;       // before event_today_08oclock
+    private static final long DATETIME_2;       // in interval of event_today_08oclock
+    private static final long DATETIME_3;       // between event_today_08oclock and event_today_15oclock
+    private static final long DATETIME_4;       // after event_today_15oclock --> no upcoming for today
 
     static {
         try {
-            DATETIME_1 = DATE_FORMAT.parse("2014-09-11T08:45:00Z").getTime();
+            DATETIME_1 = DATE_FORMAT.parse("2014-09-11T07:45:00Z").getTime();
+            DATETIME_2 = DATE_FORMAT.parse("2014-09-11T08:45:00Z").getTime();
+            DATETIME_3 = DATE_FORMAT.parse("2014-09-11T13:45:00Z").getTime();
+            DATETIME_4 = DATE_FORMAT.parse("2014-09-11T17:12:00Z").getTime();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static final long FROZEN_TIME = DATETIME_1;
+    private static final long FROZEN_TIME = DATETIME_2;
 
     public static long nowMs() {
         if (Constants.MOCK_DATA)
